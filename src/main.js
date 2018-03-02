ebb80b7fff262863ecddcda0d91c31f1e06ed6f4
louwii <link14.online@gmail.com>
2018-03-01 18:53:54 -0800

    Fix BUG with WebGL, force Canvas as WebGL is crashing with our setup (not sure WHY, TOOK HOURS TO FIGURE THAT OUT)

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import TitleScene from './TitleScene';
 import GameScene from './GameScene';
 
 let config = {
-    type: Phaser.WEBGL,
+    type: Phaser.CANVAS, // Force CANVAS otherwise the map won't load? Bug in WebGL?
     parent: 'content',
     width: 400,
-    height: 240,
+    // height: 240,
+    height: 15*32,
     scaleMode: 0, //Phaser.ScaleManager.EXACT_FIT,
     physics: {
         default: 'arcade',
