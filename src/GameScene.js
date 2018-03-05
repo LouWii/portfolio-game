10e527f0d724fab14478f4a4c120ea2b28db9281
louwii <link14.online@gmail.com>
2018-03-04 16:06:09 -0800

    finish intro animation

--- src/GameScene.js
+++ src/GameScene.js
@@ -25,7 +25,7 @@ export default class GameScene extends Phaser.Scene {
         this.add.image(1100, 300, 'sky')
         this.add.image(1700, 300, 'sky')
 
-        // Define all sprite animations we'll use
+        // Init. all animations
         makeAnimations(this)
 
         // this.add.image(400, 300, 'background-clouds')
@@ -64,49 +64,62 @@ export default class GameScene extends Phaser.Scene {
         //  maybe because the player is already a physical object ?
         // this.physics.world.enable(this.player)
 
-        // Add intro text
-        this.textIntroBackground = this.add.graphics()
-        this.textIntroBackground.fillStyle(0x000000, 1)
+        // Add intro setup
+        this.freeze = true
+
+        let textIntroBackground = this.add.graphics()
+        textIntroBackground.fillStyle(0x000000, 1)
 
         let textIntro = this.make.text({
-            x: (this.cameras.main.width)/2,
-            y: (this.cameras.main.height- 4*32)/2,
+            x: 32*2, // margin + padding
+            y: ((this.cameras.main.height- 4*32)/2), // middle of current screen
             // scaleX: 0.5,
             // scaleY: 0.5,
             // alpha: 0,
             text: 'Hi! Welcome to the gam-ish version of my portfolio.\n\nUse the arrow keys to move around, you might find some interesting items',
-            origin: { x: 0.5, y: 0.5 },
+            // origin: { x: 0.5, y: 0.5 },
             style: {
                 font: '16px Arial',
                 fill: 'white',
                 align: 'left',
-                wordWrap: { width: ( this.cameras.main.width - 4*32) }
+                wordWrap: { width: ( this.cameras.main.width - 4*32) } // screen width + 32 margin + 32 padding
             }
         })
 
-        this.textIntroBackground.fillRect(32, 1, textIntro.width+(2*32), textIntro.height+2*32)
-        this.freeze = true
+        textIntroBackground.fillRect(32, (this.cameras.main.height- 4*32)/2 - 32, textIntro.width+(2*32), textIntro.height+2*32)
 
-        this.timeBar = this.add.graphics()
-        this.timeBar.fillStyle(0x000000, 1)
-        this.timeBar.fillRect(0, 1, this.cameras.main.width, 2)
+        let timeBarBackground = this.add.graphics()
+        timeBarBackground.fillStyle(0xffffff, 1)
+        timeBarBackground.fillRect(0, ((this.cameras.main.height- 4*32)/2 - 32)+(textIntro.height+2*32 + 4), textIntro.width+(2*32), 2)
 
         this.tweens.add({
-            targets: this.timeBar,
-            x: (this.cameras.main.width),
-            ease: 'Sine.easeInOut',
-            duration: 4000,
+            targets: timeBarBackground,
+            // width: 0,
+            scaleX: 0, // Trick, animating the width doesn't work ?
+            ease: 'linear',
+            duration: 5000,
             repeat: 0,
-            onComplete: function () {_this.timeBar.visible = false}
+            onComplete: function () {
+                _this.player.lookingUp = false
+                timeBarBackground.setVisible(false)
+                textIntroBackground.setVisible(false)
+                textIntro.setVisible(false)
+                setTimeout(
+                    function(){
+                        _this.player.go = true
+                    },
+                    300
+                )
+            }
         })
 
         setTimeout(
             function(){
                 _this.freeze = false
-                _this.player.lookingUp = false
                 _this.player.canMove = true
+                _this.player.go = false
             },
-            4000
+            6300
         )
 
         // Objects setup
